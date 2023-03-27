04daeb0f929b5602431ed987fa812446aa7b9859
anjiDananto <trianji@evisi.co.id>
2023-03-27 19:41:47 +0700

    add new folder / project spring

---
+++ marketplace-practice/src/main/java/com/marketplace/practice/marketplacepractice/MarketplacePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.practice.marketplacepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplacePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplacePracticeApplication.class, args);
+	}
+
+}
---
+++ marketplace-practice/src/test/java/com/marketplace/practice/marketplacepractice/MarketplacePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.practice.marketplacepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
