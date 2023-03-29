41c432cb2253f596e911d0cbbde77112c8930815
anjiDananto <trianji@evisi.co.id>
2023-03-29 20:32:23 +0700

    Update code

---
+++ marketplace-practice/src/main/java/com/marketplace/practice/marketplacepractice/model/CategoryProduct.java
@@ -0,0 +1,27 @@
+package com.marketplace.practice.marketplacepractice.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CategoryProduct extends BaseModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int categoryId;
+
+  private String categoryName;
+
+}
