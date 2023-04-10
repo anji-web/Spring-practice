b8accca1a2387222a010bf0e0376b41fca3d2466
anjiDananto <trianji@evisi.co.id>
2023-04-10 20:21:07 +0700

    updated code CRUD Category product

--- marketplace-practice/src/main/java/com/marketplace/practice/marketplacepractice/services/CategoryServices.java
+++ marketplace-practice/src/main/java/com/marketplace/practice/marketplacepractice/services/CategoryServices.java
@@ -4,7 +4,10 @@ import com.marketplace.practice.marketplacepractice.dto.Category.Requests.Create
 import com.marketplace.practice.marketplacepractice.dto.Category.Response.CreateCategoryResponseDto;
 import com.marketplace.practice.marketplacepractice.model.CategoryProduct;
 import com.marketplace.practice.marketplacepractice.repo.CategoryRepo;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,7 +21,6 @@ public class CategoryServices {
 
 
   public CreateCategoryResponseDto  saveCategory(CreateCategoryDto createCategoryDto) throws Exception {
-
 
       if (createCategoryDto.getCategoryName().matches("[0-9]+")) {
             throw new Exception("Category name should be includes alphabetically");
@@ -46,6 +48,55 @@ public class CategoryServices {
 
   }
 
+  public List<CreateCategoryResponseDto> getDataList() {
+    List<CategoryProduct> categoryProducts = categoryRepo.findAll(); //select * from CategoryProduct
+    List<CreateCategoryResponseDto> responseDtoList = new ArrayList<>();
+
+    for (CategoryProduct cp : categoryProducts ) {
+        CreateCategoryResponseDto responseDto = new CreateCategoryResponseDto();
+        responseDto.setCategoryId(cp.getCategoryId());
+        responseDto.setCategoryName(cp.getCategoryName());
+        responseDto.setResult("Success");
+        responseDto.setStatusCode(HttpStatus.OK.value());
+        responseDtoList.add(responseDto);
+    }
+
+    return responseDtoList;
+  }
+
+
+  public CreateCategoryResponseDto getDataByCategoryId(int categoryId) {
+    CreateCategoryResponseDto responseDto = new CreateCategoryResponseDto();
+    Optional<CategoryProduct> categoryProduct = categoryRepo.findById(categoryId); //ambil data dari database menggunakan JPA
+    if (categoryProduct.isEmpty()){
+      responseDto.setResult("data not found");
+      responseDto.setCategoryId(0);
+      responseDto.setCategoryName(null);
+      responseDto.setStatusCode(HttpStatus.OK.value());
+      return responseDto;
+    }
+    responseDto.setCategoryId(categoryProduct.get().getCategoryId());
+    responseDto.setCategoryName(categoryProduct.get().getCategoryName());
+    responseDto.setStatusCode(HttpStatus.OK.value());
+    responseDto.setResult("Success");
+
+    return responseDto;
+  }
+
+  public CategoryProduct update(CategoryProduct categoryProduct) {
+        Optional<CategoryProduct> product = categoryRepo.findById(categoryProduct.getCategoryId());
+        product.get().setCategoryName(categoryProduct.getCategoryName());
+        product.get().setUpdated_at(new Date());
+        categoryRepo.save(product.get());
+        return product.get();
+  }
+
+  public String delete(int categoryId) {
+    Optional<CategoryProduct> product = categoryRepo.findById(categoryId);
+    categoryRepo.delete(product.get());
+
+    return "deleted successfully";
+  }
 
 
 
